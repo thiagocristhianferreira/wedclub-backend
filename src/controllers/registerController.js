6b930599270031040a0264d6bdf6298b37e38ba0
Thiago Ferreira <thiagocristhian@gmail.com>
2021-10-28 00:16:27 -0300

    Adicionando EsLint

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,20 +1,20 @@
-const createUser = async (req, res, next) => {
+const createUser = async (req, res, _next) => {
   res.status(200).json({ newRegister: 'result' });
 };
 
-const getAllUsers = async (_req, res, next) => {
+const getAllUsers = async (_req, res, _next) => {
   res.status(200).json({ registers: 'result' });
 };
 
-const getUserById = async (_req, res, next) => {
+const getUserById = async (_req, res, _next) => {
   res.status(200).json({ user: 'result' });
 };
 
-const editUserById = async (_req, res, next) => {
+const editUserById = async (_req, res, _next) => {
   res.status(200).json({ userEdited: 'result' });
 };
 
-const deleteUserById = async (_req, res, next) => {
+const deleteUserById = async (_req, res, _next) => {
   res.status(200).json({ userDeleted: 'result' });
 };
 
